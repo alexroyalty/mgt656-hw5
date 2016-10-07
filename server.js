c5a742b8520eb8bdb4bf805d4f5859b5e73fd148
Alex Royalty <alex.royalty@yale.edu>
2016-10-07 02:02:14 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -4,5 +4,8 @@ var app = express();
 app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
+app.get('/beautiful-locust', function(request, response){
+    response.send('This is the special page for a beautiful locust');
+});
 
 app.listen(process.env.PORT || 4000);
